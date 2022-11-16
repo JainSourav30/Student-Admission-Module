a6bc9b335ba04cad8341b04f9660828076f6c9c1
Pranav Chatur <pranavchatur@gmail.com>
2022-11-17 00:03:30 +0530

    Start getstring and getwait function

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const pool = require('./database')
 
-applicants = [
+/* applicants = [
     {
         id: 'A',
         percentile : 100,
@@ -151,14 +151,72 @@ branches = [
         status: 6,
         wl_no: 1
     },
-]
+] */
 
-const prom = new Promise((resolve, reject)=>{
-    const results = pool.query("SELECT * FROM applicants;")
+//(XXX,nnnn)
+
+ const getwait = (pref) => {
+    let str
+    for(let i=5;pref[i]!=')';i++){
+        str = str.concat(pref[i])
+    }
+    let b = parseint(str)
+    return b
+}
+
+const getstring = (pref) => {
+    let str
+        for(let i=1;pref[i]!=',';i++){
+            str = str.concat(pref[i])
+        }
+    return str
+
+}
+
+const applicants = []
+const branches = []
+
+
+/* const retrieveData = async() => {
+    const results = await pool.query("SELECT * FROM applicants;")
+    console.log(results.rows);
+}
+
+retrieveData() */
+
+/* const prom = new Promise((resolve, reject)=>{
+    const results = pool.query("SELECT UNNEST(prefs) FROM applicants;")
     resolve(results);
 })
 
-prom.then((data)=>console.log(data.rows))
+
+prom.then((data)=>console.log(data.rows[0].unnest))  */
+
+console.log(getstring("(CSE,100)"))
+
+/*
+
+INSERT INTO "public"."applicants" ("id", "percentile", "prefs", "status", "on_hold") VALUES
+(1, 100, '{"(CSE,100)","(CCE,100)","(ECE,100)"}', -1, 'f'),
+(2, 90, '{"(CSE,100)","(ECE,100)","(MME,100)"}', -1, 'f'),
+(3, 80, '{"(CCE,100)","(CSE,100)","(MME,100)"}', -1, 'f'),
+(4, 70, '{"(CCE,100)","(MME,100)","(CSE,100)"}', -1, 'f'),
+(5, 60, '{"(CSE,100)","(CCE,100)","(MME,100)"}', -1, 'f'),
+(6, 50, '{"(ECE,100)","(CSE,100)","(MME,100)"}', -1, 'f'),
+(7, 40, '{"(CSE,100)","(MME,100)","(ECE,100)"}', -1, 'f'),
+(8, 30, '{"(ECE,100)","(CCE,100)","(CSE,100)"}', -1, 'f'),
+(9, 20, '{"(CCE,100)","(CSE,100)","(ECE,100)"}', -1, 'f'),
+(10, 10, '{"(CSE,100)","(MME,100)","(ECE,100)"}', -1, 'f');
+
+INSERT INTO "public"."branches" ("id", "seats", "status", "wl_no") VALUES
+('CCE', 1, 2, 1),
+('CSE', 2, 1, 1),
+('DCS', 3, 5, 1),
+('DEC', 3, 6, 1),
+('ECE', 1, 3, 1),
+('MME', 3, 4, 1);
+*/
+
 
 
 module.exports = {applicants, branches}
